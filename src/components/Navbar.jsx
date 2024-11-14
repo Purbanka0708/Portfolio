a423518462b30a1374176c41f093d1fcd6404e21
Purbanka <purbankaghosh1708@gmail.com>
2024-11-14 20:29:28 +0530

    new

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div>
+            <div>Purbanka</div>
+            <div>
+                <a href="#home">Home</a>
+                <a href="#home">About me</a>
+                <a href="#home">Services</a>
+                <a href="#home">Projects</a>
+                <a href="#home">Contact</a>
+            </div>
+            <button></button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
