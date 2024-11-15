e13998ec083f0e31f72c7a77b8fb70a9a7feeb09
Purbanka <purbankaghosh1708@gmail.com>
2024-11-15 11:03:02 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-        <div>
-            <div>Purbanka</div>
-            <div>
-                <a href="#home">Home</a>
-                <a href="#home">About me</a>
-                <a href="#home">Services</a>
-                <a href="#home">Projects</a>
-                <a href="#home">Contact</a>
+    <div className='bg-black text-white px-8 md:px-16 lg:px-25'>
+        <div className='container py-2 flex justify-center md:justify-between items-center'>
+            <div className='text-2xl font-bold hidden md:inline'>Purbanka</div>
+            <div className='space-x-6'>
+                <a href="#home" className='hover:text-gray-500'>Home</a>
+                <a href="#about" className='hover:text-gray-500'>About me</a>
+                <a href="#sevices" className='hover:text-gray-500'>Services</a>
+                <a href="#projects" className='hover:text-gray-500'>Projects</a>
+                <a href="#contact" className='hover:text-gray-500'>Contact</a>
             </div>
-            <button></button>
+            <button className='bg-gradient-to-r from-gray-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Connect Me</button>
         </div>
     </div>
   )
