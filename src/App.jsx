b594e1d8c9845b0fa13a2226a59f178ff0a17cb2
Purbanka <purbankaghosh1708@gmail.com>
2024-11-18 12:29:42 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import About from './components/About'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Projects from './components/Projects'
 import Service from './components/Service'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
        <Hero/>
        <About/>
        <Service/>
+       <Projects/>
     </div>
   )
 }
