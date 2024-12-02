67e8b614a8c97cb1e246a5909ae4aed8ee18d14a
Purbanka <purbankaghosh1708@gmail.com>
2024-12-02 15:29:01 +0530

    new change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import HeroImage from '../assets/Hero-image.jpg'
 
 function Hero() {
+  const handleResume = () => {
+    const link = document.createElement('a');
+    link.href = 'https://drive.google.com/file/d/10HU0bmjdMlE85oWZTC0U9TEbhMVyly8l/view?usp=drive_link';
+    link.setAttribute('download', 'Purbanka_Ghosh_Resume.pdf');
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  };
   return (
     <div className='bg-black text-white text-center py-16'>
         <img src={HeroImage} alt="" className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform transition-transform duration-300 hover:scale-105'/>
@@ -11,8 +19,8 @@ function Hero() {
         </h1>
         <p className='mt-4 text-lg text-gray-300 '>I specialize in building modern and responsive web applications</p>
         <div className='mt-8 space-x-4'>
-            <button className='bg-gradient-to-r from-gray-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact with me</button>
-            <button className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Resume</button>
+            <a className='bg-gradient-to-r from-gray-400 to-blue-500 text-white hidden md:inline px-4 py-2 rounded-full' href='#contact'>Contact with me</a>
+            <button className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full' onClick={handleResume}>Resume</button>
         </div>
     </div>
   )
